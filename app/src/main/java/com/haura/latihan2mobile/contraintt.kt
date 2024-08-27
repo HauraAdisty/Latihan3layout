5d41967a4575f501112e1d435f727b0b80f03a1b
HauraAdisty <162139041+HauraAdisty@users.noreply.github.com>
2024-08-27 21:13:36 +0700

    Initial commit

---
+++ app/src/main/java/com/haura/latihan2mobile/contraintt.kt
@@ -0,0 +1,20 @@
+package com.haura.latihan2mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class contraintt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contraintt)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
